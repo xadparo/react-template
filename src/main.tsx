8b941271e2acf3fe074f0cd1e1e42a1bb684bbbb
xadparo <45482148+xadparo@users.noreply.github.com>
2021-10-12 14:49:09 +0900

    feat: react src init

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
-import React from 'react'
-import { renderToString } from 'react-dom/server'
 import express from 'express'
 import path from 'path'
 
-import App from './app'
+const app = express()
 
-const server = express()
-
-server
-  .get('/health', (req, res) => {
-    return res.status(200).end(`I'm fine.`)
-  })
-  .use(express.static(path.resolve(__dirname, 'dist')))
+app
+  .use(express.static(path.resolve(__dirname, '../dist')))
   .use(express.static(path.resolve(__dirname, '../public')))
-  .get('/', (req, res) => {
-    res.end(renderToString(<App/>))
-  })
   .listen(4000)
