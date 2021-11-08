240f8d7291067229b1711f66a326477910333b4a
xadparo <45482148+xadparo@users.noreply.github.com>
2021-11-08 15:22:42 +0900

    ci: 환경변수 주입 과정 추가

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 
 - [ ] favicon.ico 세팅
 - [ ] app.tsx를 메인으로 삼아서 개발 시작
+- [ ] .env, .env.${NODE_ENV} 세팅
--- build.js
+++ build.js
@@ -1,11 +1,25 @@
 const esbuild = require('esbuild')
 const process = require('process')
+const dotenv = require('dotenv')
 
 const argv = process.argv.slice(2)
 const argm = {}
 argv.forEach((arg) => argm[arg] = true)
 
 const watch = (argm['-w'] || false)
+
+const originEnv = { ...process.env }
+const dotenv = require('dotenv')
+dotenv.config()
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+const define = {}
+
+for (const key in process.env) {
+  if (originEnv[key]) {
+    continue
+  }
+  define[`process.env.${key}`] = JSON.stringify(process.env[key] || '')
+}
 
 const backendConfig = {
   entryPoints: ['src/main.tsx'],
@@ -30,6 +44,8 @@ const defaultConfig = {
   sourcemap: watch,
   outdir: 'dist',
   tsconfig: 'tsconfig.json',
+
+  define,
 }
 
 esbuild.build({ ...defaultConfig, ...backendConfig })
