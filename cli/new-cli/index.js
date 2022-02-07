da77dcb4e251f4fba518543e1cc7db7b7317c536
xadparo <45482148+xadparo@users.noreply.github.com>
2022-02-07 16:42:54 +0900

    chore: 불필요한 로그 제거

--- cli/new-cli/index.js
+++ cli/new-cli/index.js
@@ -5,8 +5,6 @@ module.exports = {
   flag: '-c --cli <filename>',
   event: 'cli',
   async exec(filename) {
-    console.log('holy crap', filename)
-
     const path = `./cli/${filename}`
     await fs.readdir(path).catch((err) => {
       return fs.mkdir(path)
