c0dd5753a04d544a00993571b62ece21a1b52ab3
xadparo <45482148+xadparo@users.noreply.github.com>
2022-03-14 11:54:44 +0900

    chore: unused async remove

--- cli/util.js
+++ cli/util.js
@@ -2,20 +2,20 @@ const fs = require('fs/promises')
 const { execSync } = require('child_process')
 const path = require('path')
 
-async function yarnAdd(...packages) {
+function yarnAdd(...packages) {
   return execSync(`yarn add ${packages.join(' ')}`)
 }
-async function yarnRemove(...packages) {
+function yarnRemove(...packages) {
   return execSync(`yarn remove ${packages.join(' ')}`)
 }
 
-async function fileReplace(fromPath, toPath) {
+function fileReplace(fromPath, toPath) {
   fromPath = path.resolve(fromPath)
   toPath = path.resolve(toPath)
 
   return fs.copyFile(fromPath, toPath)
 }
-async function dirRemove(fromPath) {
+function dirRemove(fromPath) {
   fromPath = path.resolve(fromPath)
 
   return fs.rmdir(fromPath, { recursive: true })
